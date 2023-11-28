02c183e100a8a306f0eaba35f7248ff848be5fa3
Dan O'Neill <dev@jdoneill.com>
2023-11-27 21:39:39 -0800

    modularize the authentication api workflow create reuseable api url funct, template out auth request

--- api/earthranger_service.go
+++ api/earthranger_service.go
@@ -32,41 +32,42 @@ type Response struct {
 // exported funtions
 // ----------------------------------------------
 
-func Authenticate(sitename, username, password string) (*Response, error) {
-	// Create a new HTTP client and make a POST request to the authentication endpoint
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s%s%s", sitename, DOMAIN, API_AUTH), nil)
+func getApiUrl(sitename string, endpoint string) string {
+	return fmt.Sprintf("https://%s%s%s", sitename, DOMAIN, endpoint)
+}
+
+func getAuthRequest(sitename string) http.Request {
+	req, err := http.NewRequest("POST", getApiUrl(sitename, API_AUTH), nil)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 	}
-
-	// Set the Content-Type and Accept headers on the request
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Accept", "application/json")
 
-	// Add the username, password, client_id, and grant_type to the request body
-	req.Body = io.NopCloser(strings.NewReader(fmt.Sprintf("username=%s&password=%s&client_id=er_mobile_tracker&grant_type=password", username, password)))
+	return *req
+}
 
-	// Make the POST request and get the response
-	res, err := client.Do(req)
+func Authenticate(sitename, username, password string) (*Response, error) {
+	client := &http.Client{}
+	authReq := getAuthRequest(sitename)
+	authReq.Body = io.NopCloser(strings.NewReader(fmt.Sprintf("username=%s&password=%s&client_id=er_mobile_tracker&grant_type=password", username, password)))
+
+	res, err := client.Do(&authReq)
 	if err != nil {
 		fmt.Println("Error making request:", err)
 	}
 	defer res.Body.Close()
 
-	// Decode the JSON response from the authentication endpoint
 	var responseData Response
 	err = json.NewDecoder(res.Body).Decode(&responseData)
 	if err != nil {
 		fmt.Println("Error decoding response:", err)
 	}
 
-	// If the request was successful, return the access token and expires in
 	if res.StatusCode == 200 {
 		return &responseData, nil
 	}
 
-	// Print out the error and error description if the request was rejected
 	fmt.Println("Error:", res.StatusCode)
 	fmt.Println("Error Description:", responseData.ErrorDescription)
 
